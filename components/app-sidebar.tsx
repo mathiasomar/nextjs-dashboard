7120b67659a8b6d82955d12ac8fc6800c13c92c4
mathiasomar <mathiasomar@gmail.com>
2025-11-12 11:39:44 +0300

    adding tooltip to sidebar button

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -93,7 +93,7 @@ const AppSidebar = () => {
             <SidebarMenu>
               {items.map((item) => (
                 <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
+                  <SidebarMenuButton tooltip={item.title} asChild>
                     <Link href={item.url}>
                       <item.icon />
                       <span>{item.title}</span>
