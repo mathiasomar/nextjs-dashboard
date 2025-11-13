c1c9a163dbccefb8de4881552472af7751f651a7
mathiasomar <mathiasomar@gmail.com>
2025-11-13 22:01:20 +0300

    changing the home cards

--- app/page.tsx
+++ app/page.tsx
@@ -19,11 +19,11 @@ export default function HomePage() {
       <div className="bg-primary-foreground p-4 rounded-lg">
         <TodoList />
       </div>
-      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
-        <AppAreaChart />
-      </div>
       <div className="bg-primary-foreground p-4 rounded-lg">
         <CardList title="Popular Content" />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        <AppAreaChart />
       </div>
     </div>
   );
